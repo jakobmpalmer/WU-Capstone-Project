c699ad4c4267373c766c7d505822b7a7c52c4885
madanielac <52931738+madanielac@users.noreply.github.com>
2019-11-13 13:30:25 -0800

    Add files via upload
    Log In Tutorial in Kotlin (Angaa Risky on YouTube).
    This shows a page with no functionality. I am trying to get the button to do something, but that isn't included in this version.

---
+++ AfterNewCourseButton.kt
@@ -0,0 +1,12 @@
+package com.example.login_tutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class AfterNewCourseButton : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
