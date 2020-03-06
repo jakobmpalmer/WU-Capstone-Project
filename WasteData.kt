b3ddf8cb5abd5fa77c0f19997dcded791cef5069
madanielac <52931738+madanielac@users.noreply.github.com>
2020-03-05 16:54:12 -0800

    Add files via upload

---
+++ WasteData.kt
@@ -0,0 +1,11 @@
+package com.example.willamette_thesis
+
+
+data class WasteData (
+    var id: String? = "",
+    var plastic_data: Int,
+    var recycle_data: Int,
+    var trash_data: Int
+
+)
+// [END blog_user_class]
