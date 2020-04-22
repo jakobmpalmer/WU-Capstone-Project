cee17acac6d978f0095a53e973603d748d6f51b6
Jakob Paulson-Palmer <jakobmpalmer@gmail.com>
2020-04-21 19:14:37 -0700

    Deleted a few unused files.

--- Willamette_Thesis/app/src/main/java/com/example/willamette_thesis/objs/DateEntry.kt
+++
@@ -1,9 +0,0 @@
-package com.example.willamette_thesis.objs
-
-data class DateEntry (
-    val date: String,
-    val time: String
-    //val waste_data : Int[],
-    //val consump_data: Int[],
-    //val travel_data: listOf()
-)
