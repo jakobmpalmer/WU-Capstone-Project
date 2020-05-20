ccf56d9258b3a08858c978c77c1a0c8d50aac1cb
Daniela Camacho <madaniela.camacho@gmail.com>
2020-05-20 09:03:30 -0700

    Update DateEntry.kt
    Cleaning up code

--- app/src/main/java/com/example/willamette_thesis/objs/DateEntry.kt
+++ app/src/main/java/com/example/willamette_thesis/objs/DateEntry.kt
@@ -3,7 +3,5 @@ package com.example.willamette_thesis.objs
 data class DateEntry (
     val date: String,
     val time: String
-    //val waste_data : Int[],
-    //val consump_data: Int[],
-    //val travel_data: listOf()
+  
 )
